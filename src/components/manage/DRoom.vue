5834a8cabb05916095109fdf12b87ba87876447f
syahraazly <73158627+Syahra-Zulya-Shania-Maghfiroh@users.noreply.github.com>
2023-02-02 10:35:23 +0700

    create and update room

--- src/components/manage/DRoom.vue
+++ src/components/manage/DRoom.vue
@@ -1,14 +1,21 @@
 <template>
 <Manage/>
 <div class="container">
-    <h3>Data Type Room </h3>
+    <div class="row">
+        <div class="col-lg-6">
+            <h3>Data Type Room </h3>
+        </div>
+        <div class="col-lg-6">
+            <button type="button" v-on:click="addRoom()" data-toggle="modal" data-target="#roomModal" class="btn btn-warning pl-5 pr-5 pt-2 pb-2 text-light">+ Add More</button>
+        </div>
+    </div>
     <div class="bg-light rounded h-100 p-4">
-        <!-- <h6 class="mb-4" v-for="type in list_type" :key="type">type.type_name</h6> -->
         <table class="table">
             <thead>
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Room Number</th>
+                    <th scope="col">Type Room</th>
                     <th scope="col">Action</th>
                 </tr>
             </thead>
@@ -16,11 +23,44 @@
                 <tr v-for="(room, index) in list_room" :key="index">
                     <th scope="row">{{index + 1}}</th>
                     <td>{{room.room_number}}</td>
+                    <td>{{room.type_name}}</td>
+                    <td>
+                        <button v-on:click="editRoom(room)" class="btn btn-warning mx-2" data-toggle="modal" data-target="#roomModal"><i class="far fa-edit"></i></button>
+                        <button v-on:click="hapus(room.room_id)" class="btn btn-danger"><i class="far fa-trash-alt"></i></button> 
+                    </td>
                 </tr>
             </tbody>
         </table>
     </div>
 </div>
+<!-- Add Modal -->
+<div class="modal fade" id="roomModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Data Type Room</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <form>
+                <!-- <div class="mb-3"> -->
+                    <input type="hidden" v-model="type_id" class="form-control" required>
+                <!-- </div>   -->
+                <div class="mb-3">
+                    <label for="">Room Number :</label>
+                    <input type="number" v-model="room_number" class="form-control" required>       
+                </div>
+                <div class="mb-3">
+                    <button v-on:click="saveData()" class="btn btn-warning">Save</button>
+                </div>
+            </form>
+        </div>
+        </div>
+    </div>
+</div>
+
 </template>
 <script>
 import Manage from '@/components/manage/Manage.vue'
@@ -29,10 +69,12 @@ export default {
     name: "DRoomView",
     data: function(){
         return{
-            type_id: '',
             list_type: [],
-            type_name: '',
             list_room: [],
+            type_id: this.$route.params.type_id,
+            room_id: "",
+            room_number: "",
+            action: "",
         }
     },
     components: {
@@ -42,7 +84,7 @@ export default {
         getRoom: function(){
             this.axios.get('/type/detail/' + this.$route.params.type_id).then(resp => {
                 this.list_room = resp.data;
-                console.log(this.list_room)
+                // console.log(this.list_room)
             })
         },
         getType: function(){
@@ -50,22 +92,51 @@ export default {
                 this.list_type = resp.data.data
             })
         },
-        // getDType(type_id){
-        //     this.axios.get('/type/' + type_id).then((resp) => {
-        //         this.type_id = resp.data.type_id
-        //         this.type_name = resp.data.desc
-        //         this.price = resp.data.price
-        //         this.desc = resp.data.type_id
-        //         // console.log(type_name);
-                
-        //     })
-        // },
+        addRoom: function(){
+            this.type_id = this.$route.params.type_id,
+            this.room_id = "",
+            this.room_number = "",
+            this.action = "insert"
+        },
+        editRoom: function(roomData){
+            this.type_id = roomData.type_id,
+            this.room_id = roomData.room_id,
+            this.room_number = roomData.room_number,
+            this.action = "update"
+        },
+        saveData: function(){
+            let form = {
+                'type_id' : this.type_id,
+                'room_number' : this.room_number
+            }
+
+            console.log(this.action);
+
+
+            if(this.action === "insert"){
+                this.axios.post('/room', form).then(resp => {
+                    this.list_type = resp.data.data
+                })
+            } else {
+                this.axios.put('/room/' + this.room_id, form).then(resp => {
+                    this.list_type = resp.data.data
+                })
+            }
+            this.getRoom()
+        },
+        hapus(room_id){
+            if(confirm('Are you sure?')){
+                this.axios.delete('/room/' + room_id).then(() =>
+                {
+                    location.reload()
+                })
+            }
+        }
         
     },
     mounted(){
-        // this.getType()
+        this.getType()
         this.getRoom()
-        // this.getDType(this.$route.params.id)
     }
 }
 </script>
